8dfa1ae6c6ae3e8823fb0bc25595808b6435743c
Nethmina01 <nethminaappdevelopment1@gmail.com>
2026-01-01 22:22:15 +0530

    complete subscription flow

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,4 +1,6 @@
 import Subscription from '../models/subscription.model.js'
+import { workflowClient } from '../config/upstash.js';
+import { SERVER_URL } from '../config/env.js';
 
 export const createSubscription = async (req, res, next)=>{
     try{
@@ -9,9 +11,42 @@ export const createSubscription = async (req, res, next)=>{
             }
         );
 
-        res.status(201).json({success:true, data:subscription});
+        const { workflowRunId } = await workflowClient.trigger({
+            url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+            body: {
+                subscriptionId: subscription.id,
+            },
+            header: {
+                "content-type": "application/json"
+            },
+            retries: 0,
+        });
+        console.log("Workflow started with ID:", workflowRunId);
+
+        res.status(201).json({success:true, data:subscription, workflowRunId});
     }
     catch(e){
+        next(e);
+    }
+}
+
+//get all subscription created by user
+
+export const getUserSubscriptions = async( req, res,next)=> {
+    try{
+        //Check if the user is the same as the one in the token
+        if(req.user.id !== req.params.id){
+            const error = new Error('You are not the Owner of this account');
+            error.status=401;
+            throw error;
+        }
+
+        const subscriptions = await Subscription.find({user:req.params.id});
+        //show the response as json and show all subscription data
+        res.status(200).json({success:true, data:subscriptions});
+
+    }
+    catch (e){
         next(e);
     }
 }
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import authorize from '../middlewares/auth.middleware.js'
-import { createSubscription } from "../controllers/subscription.controller.js";
+import { createSubscription, getUserSubscriptions } from "../controllers/subscription.controller.js";
 
 const subscriptionRouter =Router();
 
@@ -14,7 +14,7 @@ subscriptionRouter.put('/:id', (req, res)=>res.send({title:'update a subscriptio
 
 subscriptionRouter.delete('/:id', (req, res)=>res.send({title:'Delete a subscription'}));
 
-subscriptionRouter.get('/user/:id', (req, res)=>res.send({title:'Get all user subscriptions'}));
+subscriptionRouter.get('/user/:id', authorize, getUserSubscriptions);
 
 subscriptionRouter.put('/user/:id', (req, res)=>res.send({title:'Cancel subscription'}));
 
